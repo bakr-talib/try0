0322473dc196aa702ce0cc52442ea2d78dc0df2d
bakr-talib <1bakrtalib2002@gmail.com>
2024-11-11 17:39:48 +0300

    aa

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,25 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <header className="bg-blue-500 flex justify-between p-4 text-white text-1 font-bold text-lg ">
-      <h1 className="ml-7">
-        logo
-      </h1>
-      <nav className="flex justify-between">
-        <a className="ml-4" href="">aaaa</a>
-        <a className="ml-4" href="">aaaa</a>
-        <a className="ml-4" href="">aaaa</a>
-        <a className="ml-4" href="">aaaa</a>
-      </nav>
-      <button className="mr-7 bg-slate-100 p-2 rounded-md text-blue-700">sing in</button>
-    </header>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+        minus?
+      </p>
+      <button> onClick={(evt)=> console.log(evt)}click me</button>
+    </div>
   );
 }
-
-export default App;
