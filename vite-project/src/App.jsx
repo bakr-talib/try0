06212dce39af6e179852ba9395306cbdabf97a6e
bakr-talib <1bakrtalib2002@gmail.com>
2024-11-13 03:04:00 +0300

    aaa

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,21 @@
 export default function App() {
+  function handleClick(func) {
+    fub;
+  }
+
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
-        minus?
-      </p>
-      <button> onClick={(evt)=> console.log(evt)}click me</button>
-    </div>
+    <>
+      <div className="flex flex-wrap gap-[10px] md:gap-[16px]">
+ <button className="bg-[#A91079] text-[#fff] text-[12px] py-3 px-2 border-none rounded-[8px]"> python</button>
+ <button className="bg-white text-gray-400 text-[12px] py-3 px-2 border-gray-400 rounded-[8px]">python</button>
+ <button className="bg-white text-gray-400 text-[12px] py-3 px-2 border-gray-400 rounded-[8px]">python</button>
+ <button className="bg-white text-gray-400 text-[12px] py-3 px-2 border-gray-400 rounded-[8px]">python</button>
+ <button className="bg-white text-gray-400 text-[12px] py-3 px-2 border-gray-400 rounded-[8px]">python</button>
+ <button className="bg-white text-gray-400 text-[12px] py-3 px-2 border-gray-400 border-solid rounded-[8px] ">python</button>
+      </div>
+      <div>
+        <input className='text-' placeholder="text" type="text" />
+      </div>
+    </>
   );
 }
